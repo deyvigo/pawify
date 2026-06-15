0d777ec14d8db69dce2b329b93eab25a8c0c1585
BrayanAlq <brayan.alquizar@unmsm.edu.pe>
2026-06-14 22:49:28 -0500

    delete: unused .js/jsx files

--- pawify-admin/src/components/Button.jsx
+++
@@ -1,7 +0,0 @@
-export default function Button({ variant = 'primary', loading, children, ...props }) {
-  return (
-    <button className={`btn btn-${variant}`} disabled={loading} {...props}>
-      {loading ? (typeof loading === 'string' ? loading : 'Cargando...') : children}
-    </button>
-  );
-}
--- pawify-admin/src/screens/ClaimsPage.jsx
+++
@@ -1,8 +0,0 @@
-export default function ClaimsPage() {
-  return (
-    <div>
-      <h1>Gestión de Reclamos</h1>
-      <p>Gestiona los reclamos y quejas de los clientes.</p>
-    </div>
-  );
-}
